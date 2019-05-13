3d092461827b3df89d3fa1558583f931fdc41408
ZixinChen0520 <50555711+ZixinChen0520@users.noreply.github.com>
2019-05-13 00:02:10 -0400

    update main.cu
    comments

--- Project/CUDA-CNN-1D-worked/main.cu
+++ Project/CUDA-CNN-1D-worked/main.cu
@@ -24,6 +24,7 @@ static void test();
 static double forward_pass(double data[28][28]);
 static double back_pass();
 
+//load minist data and get its total number
 static inline void loaddata()
 {
 	mnist_load("data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte",
@@ -71,15 +72,18 @@ static double forward_pass(double data[28][28])
 	int datasize[1] ;
 	datasize[0] = 28;
 	l_input.setOutput((float *)input,(int *)datasize);
-
+        // convolutional layer, calculate the output, bias and give them to the activation function
 	fp_preact_c1<<<BLOCKSIZE, GRIDSIZE>>>((float*) l_input.output, (float*) l_c1.preact, (float*) l_c1.weight, l_c1.preactsize,28,5,6);
 	fp_bias_c1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_c1.preact, l_c1.bias,l_c1.preactsize, 6);
 	apply_step_function<<<BLOCKSIZE, GRIDSIZE>>>(l_c1.preact, l_c1.output, l_c1.O);
-
+        
+	// pooling layer, calculate the output
 	fp_preact_s1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_c1.output, (float*)l_s1.preact, (float*)l_s1.weight,l_s1.preactsize,l_c1.preactsize,l_c1.N,4);
 	fp_bias_s1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_s1.preact, l_s1.bias, l_s1.preactsize,l_c1.N);
 	apply_step_function<<<BLOCKSIZE, GRIDSIZE>>>(l_s1.preact, l_s1.output, l_s1.O);
 
+	
+	// dense layer, calculate the output, bias and give them to the activation function, then output (10 nodes)
 	fp_preact_f<<<BLOCKSIZE, GRIDSIZE>>>((float* )l_s1.output, l_f.preact, (float*)l_f.weight, l_s1.preactsize, l_c1.N,l_f.O);
 	fp_bias_f<<<BLOCKSIZE, GRIDSIZE>>>(l_f.preact, l_f.bias,l_f.O);
 	apply_step_function<<<BLOCKSIZE, GRIDSIZE>>>(l_f.preact, l_f.output, l_f.O);
@@ -94,21 +98,22 @@ static double back_pass()
 	clock_t start, end;
 
 	start = clock();
-
+        //there is no front information for output layer, we can just calculate the delta w and delta b
 	bp_weight_f<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_f.d_weight, l_f.d_preact, (float*)l_s1.output, l_f.O, l_c1.N,l_s1.preactsize);
 	bp_bias_f<<<BLOCKSIZE, GRIDSIZE>>>(l_f.bias, l_f.d_preact,l_f.O);
-
+        
+	// do what is mentioned in report
 	bp_output_s1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_s1.d_output, (float*)l_f.weight, l_f.d_preact, l_f.O,l_c1.N,l_s1.preactsize);
 	bp_preact_s1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_s1.d_preact, (float*)l_s1.d_output, (float*)l_s1.preact, l_c1.N,l_s1.preactsize);
 	bp_weight_s1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_s1.d_weight, (float*)l_s1.d_preact, (float*)l_c1.output, l_s1.N,4,l_c1.N,l_s1.preactsize,l_c1.preactsize);
 	bp_bias_s1<<<BLOCKSIZE, GRIDSIZE>>>(l_s1.bias, (float*)l_s1.d_preact, l_c1.N, l_s1.preactsize);
-
+        // do what is mentioned in report
 	bp_output_c1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_c1.d_output, (float*)l_s1.weight, (float*)l_s1.d_preact, l_s1.N,4,l_c1.N,l_s1.preactsize,l_c1.preactsize);
 	bp_preact_c1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_c1.d_preact, (float*)l_c1.d_output, (float*)l_c1.preact,l_c1.N,l_c1.preactsize);
 	bp_weight_c1<<<BLOCKSIZE, GRIDSIZE>>>((float*)l_c1.d_weight, (float*)l_c1.d_preact, (float*)l_input.output,l_c1.N,5,l_c1.preactsize,l_input.preactsize);
 	bp_bias_c1<<<BLOCKSIZE, GRIDSIZE>>>(l_c1.bias, (float*)l_c1.d_preact, l_c1.N, l_c1.preactsize);
 
-
+        //apply grade according to the delta w
 	apply_grad<<<BLOCKSIZE, GRIDSIZE>>>(l_f.weight, l_f.d_weight, l_f.M * l_f.N);
 	apply_grad<<<BLOCKSIZE, GRIDSIZE>>>(l_s1.weight, l_s1.d_weight, l_s1.M * l_s1.N);
 	apply_grad<<<BLOCKSIZE, GRIDSIZE>>>(l_c1.weight, l_c1.d_weight, l_c1.M * l_c1.N);
@@ -150,19 +155,19 @@ static void learn()
 
 		for (int i = 0; i < train_cnt; ++i) {
 			float tmp_err;
-
-			time_taken += forward_pass(train_set[i].data);
+            
+			time_taken += forward_pass(train_set[i].data); //forward pass
 
 			l_f.bp_clear();
 			l_s1.bp_clear();
 			l_c1.bp_clear();
 
 			// Euclid distance of train_set[i]
-			makeError<<<10, 1>>>(l_f.d_preact, l_f.output, train_set[i].label, 10);
+			makeError<<<10, 1>>>(l_f.d_preact, l_f.output, train_set[i].label, 10); //calculate error
 			cublasSnrm2(blas, 10, l_f.d_preact, 1, &tmp_err); //calculate the norm2
 			err += tmp_err;
 
-			time_taken += back_pass();
+			time_taken += back_pass(); //back propagation
 		}
 
 		err /= train_cnt;
